b671bc181fec52f726b805321d1eef995b4b5f76
harshdhanwani <harsh.dhanwani112yahoo.in>
2018-03-18 16:07:17 +0000

    Resource file to handle GET request to get comments from a photo implemented in PhotoResource.java

--- src/main/java/org/stacspics/CommentingSystem/resources/PhotoResource.java
+++ src/main/java/org/stacspics/CommentingSystem/resources/PhotoResource.java
@@ -1,4 +1,31 @@
 package org.stacspics.CommentingSystem.resources;
 
+import com.google.gson.Gson;
+import org.stacspics.CommentingSystem.entities.Comments;
+import org.stacspics.CommentingSystem.entities.Photos;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import java.util.ArrayList;
+
+@Path("/photos")
+
 public class PhotoResource {
+
+    StorageResource storageResource = new StorageResource().readFromJson("data.json");
+    Gson gson = new Gson();
+
+    @GET
+    @Path("{photoId}/comments")
+    @Produces("text/plain")
+    public String getCommentsFromPhoto(@PathParam("photoId") int photoId){
+
+        Photos photo = storageResource.getPhoto(photoId);
+        ArrayList<Comments> comments = photo.getComments();
+        return gson.toJson(comments);
+
+    }
+
 }
